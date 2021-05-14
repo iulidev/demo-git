f1b595f35022154ae31d520df5ee79de1db0afda
Iulian Staverescu <iulians6@yahoo.com>
2021-05-14 20:08:23 +0300

    First commit

---
+++ functions.py
@@ -0,0 +1,11 @@
+def cleanup(string):
+    """Sterge spatiile de la inceput si sfarsit
+    Transforma stringul in formatul prima litera mare, urmatoarele mici
+    Returneaza stringul
+    """
+    new_string = string.strip()
+    new_string = new_string.capitalize()
+    return new_string
+
+
+print(cleanup("  text de transformat"))
